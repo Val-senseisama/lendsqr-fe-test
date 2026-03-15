42fdd05b25cf262c09339488f9b0c2a15f71479e
David Val <valizevbigiedavid@gmail.com>
2026-03-15 03:27:05 +0100

    feat: add SVG assets for React and Vite, update TypeScript configurations
    - Added react.svg and vite.svg to the assets directory.
    - Created declarations for .lottie and .svg files to support React components.
    - Introduced tsconfig.app.json and tsconfig.node.json for improved TypeScript configuration.
    - Set up a root tsconfig.json to reference app and node configurations.
    - Configured Vite with React and SVGR plugins for SVG handling.

---
+++ src/declarations.d.ts
@@ -0,0 +1,15 @@
+declare module "*.lottie" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.svg?react" {
+  import React from "react";
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
